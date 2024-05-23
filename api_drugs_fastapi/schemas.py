dc1edeb1cf94bad7857b9f723d0650c0086d131a
Tatiane Weitzel <weitzeltati@gmail.com>
2024-05-23 15:24:27 -0300

    implementando as rotas CRUD

---
+++ api_drugs_fastapi/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+
+
+class Message(BaseModel):
+    message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
